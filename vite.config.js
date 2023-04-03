f17088880e86813c6cc4d9d45cec173a2bf13fec
JasurAvazov <jasuravazov4@gmail.com>
2023-04-03 06:42:17 +0500

    delete dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+// })
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import copy from 'rollup-plugin-copy'; // импортируем плагин copy
+
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    copy({ // добавляем настройки плагина copy
+      targets: [
+        { src: './index.html', dest: 'dist' }, // копируем index.html из src в dist
+        { src: 'src/assets', dest: 'dist' }, // копируем папку assets из src в dist
+      ],
+    }),
+  ],
+});
